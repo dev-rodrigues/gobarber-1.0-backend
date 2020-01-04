6c8f863339aac17b352e62cfca4e298f1036f7ce
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-01-03 23:35:12 -0300

    model user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -6,7 +6,7 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
-                password: Sequelize.STRING,
+                password_hash: Sequelize.STRING,
                 provider: Sequelize.BOOLEAN
             },
             {
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello word' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Carlos',
+        email: 'carlos.hrq@gmail.com',
+        password_hash: '12345werwerwe'
+    });
+    return res.json(user);
+});
 
 export default routes;
