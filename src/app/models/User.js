c69820faf52387c3b358dfc4f477078f839b6f0e
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-01-04 21:20:06 -0300

    criptografando senha do usuario utilizando hook beforesave

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,6 +15,12 @@ class User extends Model {
                 sequelize
             }
         );
+        this.addHook('beforeSave', async user => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+        });
+        return this;
     }
 }
 
