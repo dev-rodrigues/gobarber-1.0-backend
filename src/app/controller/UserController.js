e68cfe4b6d0870263c3ec45aa7526ccff9240e21
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-01-05 18:16:10 -0300

    validacao input cadastro e update do usuario

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
@@ -6,7 +7,25 @@ class UserController {
      *  CRIAR MIDDLEWARE PARA VALIDAR EXISTENCIA DO USUARIO E TORNAR O METODO
      *  STORE MAIS ENXUTO
      */
+
+    /**
+     * TODO:
+     * CRIAR MIDDLEWARE PARA VALIDAR INPUT DO USUARIO
+     */
     async store(req, res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().required(),
+            password: Yup.string()
+                .required()
+                .min(6)
+                .max(12)
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fail.' });
+        }
+
         const user_exists = await User.findOne({
             where: {
                 email: req.body.email
@@ -30,7 +49,33 @@ class UserController {
      * TODO:
      * Verificar se novo email está sendo utilizado
      */
+
+    /**
+     * TODO:
+     * CRIAR MIDDLEWARE PARA VALIDAR INPUT DO USUARIO
+     */
     async update(req, res) {
+        const schema = Yup.object({
+            name: Yup.string(),
+            email: Yup.string(),
+            oldPassword: Yup.string()
+                .min(6)
+                .max(12),
+            password: Yup.string()
+                .min(6)
+                .max(12)
+                .when('oldPassword', (oldPassword, field) =>
+                    oldPassword ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            )
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fail.' });
+        }
+
         // dados básicos
         const { email, oldPassword } = req.body;
 
