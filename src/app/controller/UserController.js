8859f086b1f2a658256b0500bbab2cebcfcb3012
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-01-05 16:31:44 -0300

    update user

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -27,9 +27,30 @@ class UserController {
     }
 
     async update(req, res) {
-        console.log(req.userId);
+        const { email, oldPassword } = req.body;
 
-        return res.json({ ok: true });
+        const user = await User.findByPk(req.userId);
+
+        if (email !== user.email) {
+            const user_exists = await User.findOne({ where: { email } });
+
+            if (user_exists) {
+                return res.status(400).json({ error: 'User already existis' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(400).json({ error: 'Password does not match' });
+        }
+
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provider
+        });
     }
 }
 
