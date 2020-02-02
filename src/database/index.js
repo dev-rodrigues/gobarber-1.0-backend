a4ef86e3785c41c84d0e9c410cbe7f52cccee16f
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-02-02 18:33:56 -0300

    relacionamento usuario x foto

--- src/database/index.js
+++ src/database/index.js
@@ -14,7 +14,12 @@ class Database {
 
     init() {
         this.connection = new Sequelize(databaseConfig);
-        models.map(m => m.init(this.connection));
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
