c8e9fbe4e4f7d658a174058c8fd82df0c95610e9
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-02-08 15:56:43 -0300

    paginacao

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -49,6 +49,8 @@ class AppointmentController {
     }
 
     async index(req, res) {
+        const { page = 1 } = req.query;
+
         const appointments = await Appointment.findAll({
             where: {
                 user_id: req.userId,
@@ -56,6 +58,8 @@ class AppointmentController {
             },
             order: ['date'],
             attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
             include: [
                 {
                     model: User,
