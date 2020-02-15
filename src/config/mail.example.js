8b646a3a13388e0377033e69dd1d3f609fa5ede3
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-02-15 18:02:11 -0300

    add config example mail

---
+++ src/config/mail.example.js
@@ -0,0 +1,12 @@
+export default {
+    host: '',
+    port: '',
+    secure: false,
+    auth: {
+        user: '',
+        password: ''
+    },
+    default: {
+        from: 'dos Santos <noreply@teste.com>'
+    }
+};
