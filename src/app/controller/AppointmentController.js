2a90a3065e039b0daaa3948c3ae34be03fb217cd
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-02-15 22:32:45 -0300

    update file Mail

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -5,7 +5,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../../schemas/notification';
-import mail from '../lib/mail';
+import mail from '../lib/Mail';
 
 class AppointmentController {
     async index(req, res) {
